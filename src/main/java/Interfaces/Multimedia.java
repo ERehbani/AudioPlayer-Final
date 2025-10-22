2586e3ca5d4dc0f510a72e557bc82269383a07d9
Elian Rehbani <elianrhbn@gmail.com>
2025-10-22 19:12:36 -0300

    Interface Multimedia

---
+++ src/main/java/Interfaces/Multimedia.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Multimedia {
+    boolean reproducir();
+    boolean parar();
+    boolean siguienteCancion();
+    boolean anteriorCancion();
+
+    //void cargarCancion();
+}
