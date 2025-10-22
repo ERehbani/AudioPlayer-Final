7da378e42fd0d7c3f21ea522d51f1cbd194394b8
Elian Rehbani <elianrhbn@gmail.com>
2025-10-22 19:29:09 -0300

    Clase ArchivoMultimedia

---
+++ src/main/java/com/example/audioplayerfinal/Clases/ArchivoMultimedia.java
@@ -0,0 +1,53 @@
+package com.example.audioplayerfinal.Clases;
+
+import com.example.audioplayerfinal.Interfaces.Multimedia;
+
+public class ArchivoMultimedia implements Multimedia {
+    private String nombre;
+    private int duracion;
+
+    public ArchivoMultimedia() {
+    }
+
+    public ArchivoMultimedia(String nombre, int duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+
+    @Override
+    public boolean reproducir() {
+        return false;
+    }
+
+    @Override
+    public boolean parar() {
+        return false;
+    }
+
+    @Override
+    public boolean siguienteCancion() {
+        return false;
+    }
+
+    @Override
+    public boolean anteriorCancion() {
+        return false;
+    }
+}
