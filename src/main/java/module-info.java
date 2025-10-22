dbb87f31b1b7d4d884dbc820bbcb3b2cfa0b2e4a
Elian Rehbani <elianrhbn@gmail.com>
2025-10-22 18:53:37 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.audioplayerfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.audioplayerfinal to javafx.fxml;
+    exports com.example.audioplayerfinal;
+}
