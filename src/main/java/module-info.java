f42b7efa33d0fc6e7849d1a957cda7b866e212e6
nanorz <nanitoocscs@gmail.com>
2025-11-07 22:14:00 -0300

    Reproductor de audio, barra de progreso, pausa y start, volumen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,10 +7,14 @@ module com.example.audioplayerfinal {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires org.json;
+    requires javafx.media;
 
     opens com.example.audioplayerfinal to javafx.fxml;
     opens com.example.audioplayerfinal.Clases to javafx.base;
     opens com.example.audioplayerfinal.Controllers to javafx.fxml;
+    opens com.example.audioplayerfinal.ReproduccionYBarra to javafx.graphics;
+    exports com.example.audioplayerfinal.ReproduccionYBarra;
+
 
     exports com.example.audioplayerfinal;
     exports com.example.audioplayerfinal.Interfaces;
