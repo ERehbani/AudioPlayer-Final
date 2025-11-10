18379e732320732ad6d480c2ec7ee04308104c64
Axel <bruniniaxel@gmail.com>
2025-11-10 20:20:33 -0300

    Correccion Excepciones
    Corregidas las excepciones que extendian de RunTimeExcepcion.
    Ahora todas extienden de Excepcion
    
    -Eliminacion del contenido en Launcher
    
    aguante leclerc

--- src/main/java/com/example/audioplayerfinal/Exceptions/PlaylistVaciaException.java
+++ src/main/java/com/example/audioplayerfinal/Exceptions/PlaylistVaciaException.java
@@ -1,6 +1,6 @@
 package com.example.audioplayerfinal.Exceptions;
 
-public class PlaylistVaciaException extends RuntimeException {
+public class PlaylistVaciaException extends Exception {
     public PlaylistVaciaException(String message) {
         super(message);
     }
